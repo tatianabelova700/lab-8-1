46609d3dc879733d4995d048bc8e23a3b9ba9e77
Tatyana <Tatyana@DESKTOP-74DNP3C>
2021-12-22 19:18:05 +0300

    Добавьте файлы проекта.

---
+++ lab 8/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab_8
+{
+    internal class Directory
+    {
+        static void Main(string[] args)
+        {
+            string dirName = "C:\\Program Files";
+
+            DirectoryInfo dirInfo = new DirectoryInfo(dirName);
+
+            Console.WriteLine($"Название каталога: {dirInfo.Name}");
+            Console.WriteLine($"Полное название каталога: {dirInfo.FullName}");
+            Console.WriteLine($"Время создания каталога: {dirInfo.CreationTime}");
+            Console.WriteLine($"Корневой каталог: {dirInfo.Root}");
+            Console.ReadKey();
+        }
+    }
+}
